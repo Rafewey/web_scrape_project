ed15931d54177c89cadeb8d3b63b13f3c5c55210
rafewey <rafewey@gmail.com>
2020-05-02 15:34:08 -0700

    This is my first commit on this project

---
+++ first_web_scrape.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+#Creating a txt file to write on
+f = open("/home/rafewey/Desktop/web_scrape_project/scrapped_info.txt", "w+")
+
+#Grabbing the page
+url = "http://quotes.toscrape.com/"
+response = urlopen(url)
+page_html = response.read()
+
+#Parsing
+page_soup = BeautifulSoup(page_html, "html.parser")
+quotes = page_soup.findAll("div",{"class":"quote"})
+
+for quote in quotes:
+    f.write("{quote} --- {author} \n".format(quote=quote.span.text, author=quote.small.text))
+
+response.close()
+f.close()
